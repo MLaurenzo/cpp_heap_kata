1d5017eaddbfa208d5eb232c99a38c0f751ce029
MLaurenzo <laurent.bertholle@gmail.com>
2017-09-25 19:06:44 +0200

    Print a random vector.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    vector<int> v {2, 4, 8, 9, 1, 3, 7};
+    for_each(v.begin(), v.end(), [](int i) { cout << i << " "; });
+    cout << endl;
 }
